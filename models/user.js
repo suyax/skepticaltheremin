e15244126671eb1f36fe7234a11263726e2d633c
Nikola Phipps <tesla.phipps@gmail.com>
2016-01-07 20:32:11 -0800

    added a pin model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
+var pinSchema =  require('pin.js').schema
 
 var userSchema = mongoose.schema({
-  userName: { type: String, index: { unique: true } };
-  password: String;
-  pins: Array;
-})
+  userName: { type: String, index: { unique: true } },
+  password: String,
+  pins: [pinSchema],
+});
 
 var User = mongoose.model('User', userSchema);
+
+module.exports = User;
