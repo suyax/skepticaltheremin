06d21aabb9b4db4de0dbb9c7ac269f9ae7b785e7
Lex <lex.cooke@gmail.com>
2016-01-09 11:33:45 -0800

    add click handle to GMaps instantiation in Map.js

--- public/bundle.js
+++ public/bundle.js
@@ -24504,6 +24504,20 @@
 	      el: '#map',
 	      lat: this.props.lat,
 	      lng: this.props.lng,
+	      click: function click(e) {
+	        map.addMarker({
+	          address: e.latLng.lat().toString() + e.latLng.lng().toString(),
+	          lat: e.latLng.lat(),
+	          lng: e.latLng.lng(),
+	          details: {
+	            note: "I LOVE this place."
+	          },
+	          infoWindow: {
+	            content: '<p>Dat info dohhh</p>'
+	          }
+	        });
+	        bindContext.addFavBreadCrumb(e.latLng.lat(), e.latLng.lng(), Date.now(), { note: "I LOVE this place." }, { content: '<p>Dat info dohhh</p>' });
+	      },
 	      styles: [{ "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{ "color": "#444444" }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2f2f2" }] }, { "featureType": "poi", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "saturation": -100 }, { "lightness": 45 }] }, { "featureType": "road.highway", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#46bcec" }, { "visibility": "on" }] }]
 
 	    });
