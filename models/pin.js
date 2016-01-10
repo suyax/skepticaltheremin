6474f9a55107ac9a5c5211957463e46a7288f916
mewp820 <mewpeter@gmail.com>
2016-01-09 16:57:32 -0800

    added insert updated with unique id to db

--- models/pin.js
+++ models/pin.js
@@ -1,12 +1,22 @@
 var mongoose = require('mongoose');
 
 var pinSchema = new mongoose.Schema({
+  _creator: {type: String, ref: "User"  },
   address: String,
   lat: Number,
   lng: Number,
   details: {},
   timestamp: Date
 });
+
+
+// pinSchema.pre('save', true, function(next, done) {
+//   // calling next kicks off the next middleware in parallel
+  
+
+//   next();
+//   setTimeout(done, 100);
+// });
 
 var Pin = mongoose.model('Pin', pinSchema);
 
