31cf689e9af34bb738d225f3dda9c137d1a9593b
Ian Liu <ianliu09@gmail.com>
2016-01-11 15:42:55 -0800

    Modified mongo address for deployment

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var userController = require('./controllers/userControllers.js');
 var app = express();
 var port = process.env.PORT || 3000;
 
-// var MONGO_DB = "mongodb://mewpeter:Peirce234@ds037415.mongolab.com:37415/breadcrumbs";
-var MONGO_DB;
+var MONGO_DB = "mongodb://mewpeter:Peirce234@ds037415.mongolab.com:37415/breadcrumbs";
+// var MONGO_DB;
 mongoose.connect(MONGO_DB || 'mongodb://localhost/maps');
 
 app.use(bodyParser.urlencoded({extended: true}));
