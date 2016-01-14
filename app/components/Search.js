ee5bdce0faa4358b0538c82397910be46e2d993f
suya xu <tataxusuya@hotmail.com>
2016-01-13 23:35:23 -0800

    Style enhancement: public/assets/styles.css Add select drop-down button : app/components/Search.js

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,5 +1,5 @@
 var React = require('react');
-
+//search bar
 var Search = React.createClass({
 
   getInitialState() {
@@ -11,9 +11,9 @@ var Search = React.createClass({
   },
 
   handleSubmit(event){
-    
+
     event.preventDefault();
-    
+
     // When the form is submitted, call the onSearch callback that is passed to the component
 
     this.props.onSearch(this.state.value, null, true);
@@ -25,14 +25,26 @@ var Search = React.createClass({
   render() {
 
     return (
+      //https://facebook.github.io/react/docs/forms.html
       <form id="geocoding_form" className="form-horizontal" onSubmit={this.handleSubmit}>
         <div className="form-group">
           <div className="col-xs-12 col-md-6 col-md-offset-3">
             <div className="input-group">
-              <input type="text" className="form-control" id="address" placeholder="Find a location..." 
+              <div className="styled-select">
+              <label>Crime Categories</label>
+                <select>
+                    <option value="Assault">Assault</option>
+                    <option value="Theft/Larceny">Theft/Larceny</option>
+                    <option value="Burglary">Burglary</option>
+                    <option value="Vandalism">Vandalism</option>
+                    <option value="Drugs/alcohol Violations">Drugs/alcohol Violations</option>
+                    <option value="Motor Vehicle Theft">Motor Vehicle Theft</option>
+                </select>
+              </div>
+              <input type="text" className="form-control" id="address" placeholder="Find a location..."
               value={this.state.value} onChange={this.handleChange} />
               <span className="input-group-btn">
-                <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
+              <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
               </span>
             </div>
           </div>
